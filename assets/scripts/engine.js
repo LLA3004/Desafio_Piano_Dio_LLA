4ad4d68449c4a97f2ef4035aeedb8f7976eafd29
Lucas Lyra <lucas10lyra@gmail.com>
2024-11-04 17:44:57 +0000

    sombreamento ao digitar a tecla com o teclado, adicionado o controle de volume no input

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -1,20 +1,37 @@
 const teclasPiano = document.querySelectorAll(".piano-keys .key ")
 
+const volumeSlider = document.querySelector(".volume-slider input")
+let mapedKeys = []
 let audio = new Audio("assets/tunes/a.wav");
 
 const playTune = (key) => {
     
    audio.src=`assets/tunes/${key}.wav`
     audio.play();
+   
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(()=>{
+        clickedKey.classList.remove("active")
+    },150)
 };
 
 teclasPiano.forEach((key) => {
     console.log(key.dataset.key);
-    key.addEventListener("click", () => playTune(key.dataset.key));
+    key.addEventListener("click", () =>playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key)
 });
 
 
 
 document.addEventListener("keydown",(e)=>{
-    console.log(e.key)
+    if(mapedKeys.includes(e.key)){
+    playTune(e.key)
+    }
 })
+
+const handleVolume = (e)=>{
+    audio.volume = e.target.value
+    
+}
+volumeSlider.addEventListener("input", handleVolume   )
